486148bcb90bfa1d6c79d86d36ef7e2f227c9405
99003546 <78855186+99003546@users.noreply.github.com>
2021-03-07 10:51:12 +0530

    testcases added

---
+++ LibraryManagement/LibraryManagementTests/LibrarianTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Tests
+{
+    [TestClass()]
+    public class LibrarianTests
+    {
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Librarian librarianObj = new Librarian();
+            librarianObj.Login("user", "pass");
+            Assert.AreEqual(1, 1);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void LoginTest1()
+        {
+            Librarian librarianObj = new Librarian();
+            librarianObj.Login("asdfghj", "password@12345");
+            Assert.AreEqual(-1, -1);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void LoginFailTest()
+        {
+            Librarian librarianObj = new Librarian();
+            librarianObj.Login("asdfghj", "password@12345");
+            Assert.AreEqual(1, -1);
+            Assert.IsTrue(true);
+        }
+    }
+}
